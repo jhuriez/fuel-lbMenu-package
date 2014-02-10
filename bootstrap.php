0d3fd8048cf7a325a2cc0511cd66458e87b9102a
Julien Huriez <syntaxlb@gmail.com>
2014-02-10 17:01:15 +0100

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -8,7 +8,7 @@
  * @author     Julien Huriez
  * @license    MIT License
  * @copyright  2013 Julien Huriez
- * @link   https://github.com/jhuriez/fuel-lbMenu
+ * @link   https://github.com/jhuriez/fuel-lbMenu-package
  */
 Autoloader::add_core_namespace('LbMenu');
 

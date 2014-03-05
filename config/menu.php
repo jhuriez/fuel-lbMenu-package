e26835be09ace6cd902661c697911601ed64a290
jhuriez <syntaxlb@gmail.com>
2014-03-05 11:49:35 +0100

    [UPDATE] Config file, add Boostrap theme

--- config/menu.php
+++ config/menu.php
@@ -4,6 +4,8 @@ return array(
 	'theme_default' => 'default',
 	'theme_fallback' => 'default',
 	'themes' => array(
+
+		// Default simple HTML Menu
 		'default' => array(
 	        'menu' => '<ul class="menu">{menu}</ul>',
 	        'menu_item' => '<li class="item depth-1 {active}">{item} {submenu}</li>',
@@ -13,14 +15,38 @@ return array(
 	        'sub_menu_item' => '<li class="sub-item depth-{depth} {active}">{item} {submenu}</li>',
 	        'sub_menu_item_inner' => '<a href="{link}" title="{title}">{text}</a>',
 		),
+
+		// Menu navbar for Bootstrap 2
+		'bootstrap-2' => array(
+			'menu' => '<ul class="nav">{menu}</ul>',
+			'menu_item' => '<li class="{active}">{item} {submenu}</li>',
+			'menu_item_with_children' => '<li class="{active} dropdown">{item} {submenu}</li>',
+
+			'menu_item_inner' => '<a href="{link}" title="{title}">{text}</a>',
+			'menu_item_inner_with_children' => '<a href="{link}" title="{title}" class="dropdown-toggle" data-toggle="dropdown">{text} <b class="caret"></b></a>',
+		
+			'sub_menu' => '<ul class="dropdown-menu">{menu}</ul>',
+			'sub_menu_item' => '<li class="{active}">{item} {submenu}</li>',
+			'sub_menu_item_inner' => '<a href="{link}" title="{title}">{text}</a>',
+		),
+
+		// Menu navbar for Bootstrap 3
+		'bootstrap-3' => array(
+			'menu' => '<ul class="nav navbar-nav">{menu}</ul>',
+			'menu_item' => '<li class="{active}">{item} {submenu}</li>',
+			'menu_item_with_children' => '<li class="{active} dropdown">{item} {submenu}</li>',
+
+			'menu_item_inner' => '<a href="{link}" title="{title}">{text}</a>',
+			'menu_item_inner_with_children' => '<a href="{link}" title="{title}" class="dropdown-toggle" data-toggle="dropdown">{text} <b class="caret"></b></a>',
+		
+			'sub_menu' => '<ul class="dropdown-menu">{menu}</ul>',
+			'sub_menu_item' => '<li class="{active}">{item} {submenu}</li>',
+			'sub_menu_item_inner' => '<a href="{link}" title="{title}">{text}</a>',
+		),
+
+		// Menu for Sb-admin template
 		'sb-admin' => array(
-	        'menu' => '<nav class="navbar-default navbar-static-side" role="navigation">
-	        				<div class="sidebar-collapse">
-	        					<ul class="nav" id="side-menu">
-	        						{menu}
-        						</ul>
-    						</div>
-						</nav>',
+	        'menu' => '<ul class="nav" id="side-menu">{menu}</ul>',
 						
 	        'menu_item' => '<li class="{active} {item-class}">{item} {submenu}</li>',
 	        'menu_item_inner' => '<a href="{link}" title="{title}"><i class="fa {b-icon} fa-fw"></i> {text}</a>',
